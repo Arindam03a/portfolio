61fff7bbc7f7277ddc8f73ba7bf564f74faf3490
Arindam Kotal <138249469+Arindam03a@users.noreply.github.com>
2025-03-03 23:03:41 +0530

    Delete portfloio directory

--- portfloio/script.js
+++
@@ -1,9 +0,0 @@
-document.querySelectorAll('nav a').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault(); // Prevent default anchor behavior
-        const targetId = this.getAttribute('href'); // Get the target section ID
-        document.querySelector(targetId).scrollIntoView({
-            behavior: 'smooth' // Smooth scroll to the target section
-        });
-    });
-});
